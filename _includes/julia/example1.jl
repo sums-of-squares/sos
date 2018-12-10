3377beee8bdfed8e56a81eebf117975901ccdb41
Benoît Legat <benoit.legat@gmail.com>
2018-12-10 17:57:11 +0100

    Add status variable in Example 1

--- _includes/julia/example1.jl
+++ _includes/julia/example1.jl
@@ -5,5 +5,6 @@ p = 2*x^4 + 2*x^3*y - x^2*y^2 + 5*y^4
 # We want constraint `p` to be a sum of squares
 @constraint model p >= 0
 
-# Solution status is `OPTIMAL` which means `p` is a sum of squares
-solve(model)
+status = solve(model)
+# Solution status is `Optimal` which means `p` is a sum of squares
+@show status
--- _includes/julia/example1_motzkin.jl
+++ _includes/julia/example1_motzkin.jl
@@ -1,5 +1,6 @@
 @polyvar x y
 p = x^4*y^2 + x^2*y^4 - 3*x^2*y^2 + 1
 @constraint(model, p >= 0)
-solve(model)
+status = solve(model)
 # Solution status is `Infeasible`
+@show status
