a8d126b5c37888b02a2ef6f9dbdcfe1e5f8b2847
Chenyang Yuan <yuanchenyang@gmail.com>
2021-10-25 10:40:52 -0700

    Changed Julia example's maxdegree

--- _includes/julia/example4.jl
+++ _includes/julia/example4.jl
@@ -1,9 +1,7 @@
 @polyvar x y
 @variable(model, t)
-# We need to convert each equality to two inequalities
-# to avoid numerical issues...
-S = @set x^2 >= x && y^2 >= y && x^2 <= x && y^2 <= y
-@constraint(model, x - y >= t, domain = S)
+S = @set x^2 == x && y^2 == y
+@constraint(model, x - y >= t, domain = S, maxdegree=2)
 @objective(model, Max, t)
 optimize!(model)
 println("Solution: $(value(t))")
